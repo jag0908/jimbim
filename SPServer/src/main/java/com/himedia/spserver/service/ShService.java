7408fbe9835cc37913949c2cd1b17322e3960941
kjj <128561413+wjdwls8520@users.noreply.github.com>
2025-11-06 14:39:17 +0900

    .env 파일

---
+++ SPServer/src/main/java/com/himedia/spserver/service/ShService.java
@@ -0,0 +1,30 @@
+package com.himedia.spserver.service;
+
+import com.himedia.spserver.entity.SH.SH_post;
+
+import com.himedia.spserver.repository.ShRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+
+@Service
+@Transactional
+public class ShService {
+
+    @Autowired
+    ShRepository sr;
+
+    public ArrayList<SH_post> getShList() {
+        ArrayList<SH_post> srList = sr.findAllByPost_id();
+
+        return srList;
+    }
+
+
+
+
+
+
+}

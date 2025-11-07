bc2d7305b6f8add2f7c79c029832ac6fcbf0b335
serckritt <cytus@gmail.com>
2025-11-07 10:30:26 +0900

    userSlice 수정

--- client/src/store/userSlice.js
+++ client/src/store/userSlice.js
@@ -6,6 +6,7 @@ const cookies = new Cookies()
 const initialState={
     userid:'',
     pwd:'',
+    member_id:'',
     name:'',
     email:'',
     phone:'',
@@ -28,6 +29,7 @@ const getLoginUser=()=>{
     if( member && member.userid ){
         member.userid = decodeURIComponent( member.userid );
         member.pwd = decodeURIComponent( member.pwd );
+        member.member_id = decodeURIComponent( member.member_id );
         member.name = decodeURIComponent( member.name );
         member.phone = decodeURIComponent( member.phone );
         member.email = decodeURIComponent( member.email );
@@ -54,6 +56,7 @@ export const userSlice = createSlice(
             loginAction : (state, action)=>{
                 state.userid = action.payload.userid;
                 state.pwd = action.payload.pwd;
+                state.member_id = action.payload.member_id;
                 state.name = action.payload.name;
                 state.email = action.payload.email;
                 state.phone = action.payload.phone;
@@ -72,6 +75,7 @@ export const userSlice = createSlice(
             logoutAction : (state)=>{
                 state.userid = '';
                 state.pwd  = '';
+                state.member_id  = '';
                 state.name = '';
                 state.email = '';
                 state.phone = '';

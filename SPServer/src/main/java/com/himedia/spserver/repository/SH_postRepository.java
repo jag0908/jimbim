fd1f7068a0460ca1af70fad4315a59fb35e55823
jag0908 <223416683+jag0908@users.noreply.github.com>
2025-11-26 16:52:39 +0900

    11.26 - together

--- SPServer/src/main/java/com/himedia/spserver/repository/SH_postRepository.java
+++ SPServer/src/main/java/com/himedia/spserver/repository/SH_postRepository.java
@@ -5,6 +5,7 @@ import com.himedia.spserver.entity.SH.SH_post;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Collection;
 import java.util.List;
@@ -12,7 +13,16 @@ import java.util.List;
 public interface SH_postRepository extends JpaRepository<SH_post,Integer> {
     List<SH_post> findAllByMember(Member member);
 
-    List<SH_post> findByTitleContaining(String key);
+    @Query("SELECT p FROM SH_post p JOIN FETCH p.member WHERE p.title like %:key% ")
+    List<SH_post> findByTitleContainingWithMember(String key);
 
-    Page<SH_post> findByTitleContaining(String key, Pageable pageable);
+    @Query("SELECT p FROM SH_post p JOIN FETCH p.member WHERE p.title like %:key% ")
+    Page<SH_post> findByTitleContainingWithMember(String key, Pageable pageable);
+
+
+    @Query("SELECT p FROM SH_post p JOIN FETCH p.member")
+    List<SH_post> findWithMember();
+
+    @Query("SELECT p FROM SH_post p JOIN FETCH p.member")
+    Page<SH_post> findWithMember(Pageable pageable);
 }
